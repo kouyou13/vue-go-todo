87d627a58be8d0219931894b8a1df90d859e0178
Koyo Kinoshita <58977709+kouyou13@users.noreply.github.com>
2025-12-16 15:42:07 +0900

    todo作成時間の追加

--- backend/main.go
+++ backend/main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"net/http"
-	"strconv"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 // Task データ構造の定義
 type Task struct {
-	ID        int    `json:"id"`
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
+	ID         string    `json:"id"`
+	Created_at time.Time `json:"created_at"`
+	Title      string    `json:"title"`
+	Completed  bool      `json:"completed"`
 }
 
 // メモリ上のデータベース（簡易的なストレージ）
+var timezone = time.FixedZone("Asia/Tokyo", 9*60*60)
 var tasks = [] Task {
-	{ID: 1, Title: "Goの勉強", Completed: false},
-	{ID: 2, Title: "Vue.jsのセットアップ", Completed: true},
+	{ID: "e9f9c03e-d36b-470a-8735-c79e3cb848a3", Created_at: time.Date(1900, 1, 1, 0, 0, 0, 0, timezone), Title: "Goの勉強", Completed: false},
+	{ID: "380153cc-3677-45c2-b122-8744075c9011", Created_at: time.Date(1900, 1, 1, 0, 0, 0, 0, timezone), Title: "Vue.jsのセットアップ", Completed: true},
 }
-var nextID = 3
 
 func main() {
 	r := gin.Default()
@@ -61,20 +63,15 @@ func createTask(c *gin.Context) {
 		return
 	}
 
-	newTask.ID = nextID
-	nextID++
+	newTask.ID = uuid.NewString()
+	newTask.Created_at = time.Now()
 	tasks = append(tasks, newTask)
 	c.JSON(http.StatusCreated, newTask)
 }
 
 // タスクの更新 (完了状態のトグルなど)
 func updateTask(c *gin.Context) {
-	idParam := c.Param("id")
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
-		return
-	}
+	id := c.Param("id")
 
 	var updatedTask Task
 	if err := c.ShouldBindJSON(&updatedTask); err != nil {
@@ -96,12 +93,7 @@ func updateTask(c *gin.Context) {
 
 // タスクの削除
 func deleteTask(c *gin.Context) {
-	idParam := c.Param("id")
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
-		return
-	}
+	id := c.Param("id")
 
 	for i, task := range tasks {
 		if task.ID == id {
